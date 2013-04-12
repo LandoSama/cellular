62f8a41e1e48c0112e663d02f27f6965d6a40cc3
Dalen W. Brauner <DalenWBrauner@gmail.com>
2013-04-11 20:28:43 -0400

    Removed a "number of ticks" criteria from main (so we can just run it until we're done staring at it).

--- main.py
+++ main.py
@@ -12,21 +12,18 @@ def main():
 	if len(sys.argv) == 2 and sys.argv[1] == '-':
 		starting_food_count = 100
 		starting_cell_count = 100
-		number_of_test_ticks = 1000
 	elif len(sys.argv) == 4:
 		starting_food_count  = int(sys.argv[1])
 		starting_cell_count  = int(sys.argv[2])
-		number_of_test_ticks = int(sys.argv[3])
 	else:
 		starting_food_count = input('Enter starting amount of food: ')
 		starting_cell_count = input('Enter starting amount of cells: ')
-		number_of_test_ticks = input('Enter number of test ticks: ')
 	World = environment.Environment(starting_food_count,starting_cell_count)
 	
 	# dis is a thread
 	dis = display.display(World)
 
-	for i in range(number_of_test_ticks):
+	while True:
 		# if the user exited pygame, close the rest of the program
 		if dis.isAlive() ==False:
 			sys.exit()
