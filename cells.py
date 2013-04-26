8865b4ed4c9d5dbb75116ac4f7e075c65bbb6a0b
Davidhw <David.Weinstein@ncf.edu>
2013-04-25 22:03:05 -0400

    .

--- cells.py
+++ cells.py
@@ -32,7 +32,7 @@ class Cell:
 		self.task		 = None
 		self.destination	 = None
 		self.destination_type	 = None
-		self.radius		 = ( 3.0*self.mass*self.density / (4.0*math.pi) )**(1/3.0)
+		self.radius		 = ( 3.0*self.mass*self.density / (4.0*math.pi) )**(1/2.0)
 		self.energy		 = energy
 		self.sight_range	 = .2 + self.radius
 
