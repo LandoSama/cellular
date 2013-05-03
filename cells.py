871c81a759934f35a183e24a7b4ae3e13974977b
Jack Rogers <jack.rogers@ncf.edu>
2013-05-02 21:30:05 -0300

    Update cells.py

--- cells.py
+++ cells.py
@@ -75,7 +75,8 @@ class Cell:
 		"""What the cell does when it has found food and is attempting to get it."""
 		# If there exists some food item at the destination location,
 		SIGHT_RANGE = 0.05 + self.radius
-		closest_food = environment.Environment().food_at(self.pos, SIGHT_RANGE)
+		close_food = environment.Environment().food_at(self.pos, SIGHT_RANGE)
+		closest_food = min(close_food, key = partial(reduce, call, (attrgetter("pos"), attrgetter("distance_to"), partial(call, self.pos))))# food: self.pos.distance_to(food.pos))
 		self.destination = closest_food.pos		
 		if len(environment.Environment().food_at(self.destination, 0)) != 0:
 			distance_to_destination = self.pos.distance_to(self.destination)
